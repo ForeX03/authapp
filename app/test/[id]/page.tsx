8bcb4e42f23bd93902859b9e910724dae6f2b23c
ForeX03 <kofdextv@gmail.com>
2025-05-20 17:18:00 +0200

    2

--- app/test/[id]/page.tsx
+++ app/test/[id]/page.tsx
@@ -1,5 +1,9 @@
+import prisma from "@/lib/prisma";
+
 export default async function Page({params}: {params: Promise<{id: string}>}) {
     const { id } = await params;
+
+    const { post } = await prisma.user.fin
     return (
         <>
         <h1>Test</h1>
